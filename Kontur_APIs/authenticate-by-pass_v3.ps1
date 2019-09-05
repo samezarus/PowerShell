ed505b8322379822b91dc76aefdf11ab49107772
sameza <samezarus@gmail.com>
2019-09-05 10:37:43 +1000

    с

---
+++ Kontur_APIs/authenticate-by-pass_v3.ps1
@@ -0,0 +1,7 @@
+# Мануал https://kontur-ofd-api.readthedocs.io/ru/latest/index.html
+
+# Площадки (https://kontur-ofd-api.readthedocs.io/ru/latest/Endpoints.html)
+# тестовая https://ofd-project.kontur.ru:11002/
+# боевая   https://ofd-api.kontur.ru/
+
+Clear
