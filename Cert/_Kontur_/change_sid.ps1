d4eaf932b68bc13b196e1d45bd236c17389ab009
sameza <samezarus@gmail.com>
2019-12-17 13:59:57 +1000

    Add files via upload
    c

---
+++ Cert/_Kontur_/change_sid.ps1
@@ -0,0 +1,53 @@
+# sameza
+#
+# Скрипт копирует в реестр ключи из папки $importPath
+# 
+
+clear
+
+#[Console]::OutputEncoding = [System.Text.Encoding]::GetEncoding("windows-1251")
+#[Console]::OutputEncoding = [System.Text.Encoding]::GetEncoding("windows-1251")
+#[Console]::OutputEncoding = [System.Text.Encoding]::GetEncoding("cp866")
+
+$importPath = $PSScriptRoot+'\kontur_keys'
+
+# Получение имени текущей учетной записи
+$userName = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
+
+# Получение SID текущего пользователя
+$objUser = New-Object System.Security.Principal.NTAccount($userName)
+$userSID = $objUser.Translate([System.Security.Principal.SecurityIdentifier])
+
+$userName
+$userSID.value
+
+$cryptoProUsers = 'HKLM:\SOFTWARE\Wow6432Node\Crypto Pro\Settings\USERS\'
+$userPath       = $cryptoProUsers + $userSID
+
+# --- Изменяем SID-ы в импортируемых сертификатах? 
+$keysFileList = Get-Childitem -Path $importPath
+
+foreach ($item in $keysFileList)
+{
+    $keyFilePath = $item.FullName
+    $keyFile     = Get-Content $keyFilePath
+    $findTerm    = '[HKEY_LOCAL_MACHINE'
+
+    foreach ($line in $keyFile)
+    {
+        $findIndex = $line.IndexOf($findTerm)
+        if ($findIndex -gt -1)
+        {
+            $oldSID = $line.split('\')
+            $oldSID = $oldSID[6]
+            break        
+        }
+    }
+    #
+    if ($oldSID -ne '')
+    {
+        $keyFile | ForEach-Object {$_ -replace $oldSID, $userSID.value} | Set-Content $keyFilePath
+        $oldSID
+        $userSID.value
+    }
+}
