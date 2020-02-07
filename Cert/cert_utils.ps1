6ef522d78fbc7256bad0477947e69de05bec88b6
sameza <samezarus@gmail.com>
2020-02-07 16:51:25 +1000

    Update cert_utils.ps1
    Глобальное изменение, что бы всё заработало )

--- Cert/cert_utils.ps1
+++ Cert/cert_utils.ps1
@@ -1,21 +1,25 @@
-# sameza
+clear
+
+$certsFolder = $PSScriptRoot+'\certs' # Каталог выгрузки сертификатов
 #
-# В оснастке Крипто ПРО удалить все считыватели кроме "Реестр" что бы максимально сократить количество окон выскакивающих при добавлении
+$sortFolder = $PSScriptRoot+'\sort_certs' # Каталог для отсортированных сертификатов
 #
-# Ветка на форуме https://www.cryptopro.ru/forum2/default.aspx?g=posts&t=17557
-
-Clear
-
-$certsLocation = 'cert:\CurrentUser\My'
-#$certsFolder   = $PSScriptRoot
-$certsFolder   = 'c:\temp'
-$mypwd         = ConvertTo-SecureString -String 'qwerty' -Force -AsPlainText
-
+$userName = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name # Получение имени текущей учетной записи
+#
+# Получение SID текущего пользователя
+$objUser = New-Object System.Security.Principal.NTAccount($userName)
+$userSID = $objUser.Translate([System.Security.Principal.SecurityIdentifier])
+#
+# Получение пути к ветке с ключами  текущего пользователя
+$cryptoProUsers = 'HKLM:\SOFTWARE\Wow6432Node\Crypto Pro\Settings\USERS\'
+$userKeysPath   = $cryptoProUsers + $userSID + '\Keys'
+#
+$certsLocation = 'cert:\CurrentUser\My' # Путь к хранилищу "Личное"
+#
 $zabbixSender = 'C:\Zabbix Agent\bin\win64\zabbix_sender.exe'
 $zabbixServer = '192.168.10.209'
 $senderHost   = 'vl20-cert'
 $trapName     = 'certs_expiring'
-
 
 #################################################################################################################################
 #################################################################################################################################
@@ -35,82 +39,63 @@ function clear_string($s)
     return $result
 }
 #################################################################################################################################
-function get_current_user_certs()
+function time_stamp()
 {
-    return Get-ChildItem -Path $certsLocation -Recurse #
-    
-    #foreach ($item in $certsList)
-    #{
-        #$item | Get-Member -memberType *property
-    #}
+    $result = (Get-Date -format 'yyyyMMddHHmmssfff').ToString()
+    return $result
 }
 #################################################################################################################################
-function export_certs_from_current_user($certs, $certsFolder)
-{
-    foreach ($item in $certs)
-    {
-        Export-PfxCertificate -Cert $item -FilePath ($certsFolder+'\'+$item.SerialNumber+'.pfx') -Password $mypwd
-    }
-}
-#################################################################################################################################
-function find_cert_by_serial_number($certs, $serialNumber)
+function create_folder($folderName)
 {
     $result = $false
-    
-    foreach ($item in $certs)
+
+    if (($folderName -ne '') -and ($folderName -ne $null))
     {
-        if ($item.SerialNumber -eq $serialNumber)
-        {
-            $result = $true
-            return $result
-            break
-        }
+        $x      = New-Item -Path $folderName -ItemType 'directory' -Force
+        $result = $true
     }
 
     return $result
 }
 #################################################################################################################################
-function import_certs_to_current_user($certs, $certsFolder)
+function export_certs()
 {
-    $certsFileList =  Get-ChildItem $certsFolder
+    # Экспорт сертификатов текущего пользователя из ветки реестра в каталог $certsFolder
 
-    foreach ($item in $certsFileList)
+    $x = create_folder -folderName $certsFolder
+
+    $userKeys = Get-ChildItem -Path $userKeysPath
+
+    foreach ($item in $userKeys)
     {
-        $indexPfx = $item.Name.IndexOf('.pfx')
-        if ($indexPfx -gt -1)
-        {
-            $certFile = $certsFolder+'\'+$item.Name
-            $serialNumber = $item.Name.split('.')[0]
-            
-            $fr = find_cert_by_serial_number -certs $certs -serialNumber $serialNumber
-
-            if ($fr -ne $true)
-            {
-                Import-PfxCertificate -FilePath $certFile -CertStoreLocation $certsLocation -Password $mypwd -Exportable
-            }
-        }
+	    $keyPath = $item.Name
+	
+	    $ts = time_stamp
+        $exportFilePath = $certsFolder+'\' + $ts + '.reg'
+	    reg.exe export $keyPath $exportFilePath "/y"
     }
 }
 #################################################################################################################################
-function remove_all_certs($certs)
+function get_certs_list()
 {
+    # Получение списка сертификата из хранилища "Личное"
+
+    return Get-ChildItem -Path $certsLocation -Recurse
+}
+#################################################################################################################################
+function remove_all_certs()
+{
+    # Удаляем все публичные и приватные ключи
+
+    $certs = get_certs_list
+
     # Удаляем открытые ключи
     foreach ($item in $certs)
     {
-
         $item | Remove-Item -Force -Recurse 
     }
 
     # Удаляем приватные ключи
-    #   Получение имени текущей учетной записи
-    $userName = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
-
-    #   Получение SID текущего пользователя
-    $objUser = New-Object System.Security.Principal.NTAccount($userName)
-    $userSID = $objUser.Translate([System.Security.Principal.SecurityIdentifier])
-
-    $cryptoProUsers = 'HKLM:\SOFTWARE\Wow6432Node\Crypto Pro\Settings\USERS\'
-    $userKeysPath = $cryptoProUsers + $userSID + "\Keys"
 
     $userKeys = Get-ChildItem -Path $userKeysPath
 
@@ -118,6 +103,58 @@ function remove_all_certs($certs)
     {
         #$item.Name
         reg.exe delete $item.Name /f
+    }
+}
+#################################################################################################################################
+function import_cert_from_reg_file($regFile)
+{
+    $keyFilePath = $regFile.FullName
+    $keyFile     = Get-Content $keyFilePath
+    $findTerm    = '[HKEY_LOCAL_MACHINE'
+
+    foreach ($line in $keyFile)
+    {
+        $findIndex = $line.IndexOf($findTerm)
+        if ($findIndex -gt -1)
+        {
+            $temp   = $line.split('\')
+            $oldSID = $temp[6]
+            
+            $orgStr = $temp[8]
+            $orgStr = $orgStr.split(']')
+            $orgStr = $orgStr[0]
+
+            break        
+        }
+    }
+
+    if ($oldSID -ne '')
+    {        
+        # Меняем сид на сид текущего пользователя 
+        if ($oldSID -ne $userSID.value) 
+        {
+            $keyFile | ForEach-Object {$_ -replace $oldSID, $userSID.value} | Set-Content $keyFilePath
+        }
+
+        # Устанавливаем сертификат закртой части ключа в реестр
+        reg import $keyFilePath
+
+        # Устанавливаем сертификат открытой части ключа в реестр
+        $ts = time_stamp
+        $cont = '\\.\REGISTRY\' + $orgStr
+        &'C:\Program Files\Crypto Pro\CSP\csptest.exe' -property -cinstall -cont $cont
+    }
+}
+#################################################################################################################################
+function import_certs_from_reg_files()
+{
+    # Импортирование всех сертификатов текущему пользователю из каталога $certsFolder
+
+    $regFilesList = Get-Childitem -Path $certsFolder
+    
+    foreach ($item in $regFilesList)
+    {
+        import_cert_from_reg_file -regFile $item
     }
 }
 #################################################################################################################################
@@ -221,7 +258,7 @@ function ConvertTo-Encoding ([string]$From, [string]$To)
     Begin
     {  
         $encFrom = [System.Text.Encoding]::GetEncoding($from)  
-        $encTo = [System.Text.Encoding]::GetEncoding($to)  
+        $encTo   = [System.Text.Encoding]::GetEncoding($to)  
     }  
     Process
     {  
@@ -239,9 +276,12 @@ function send_to_zabbix ($tName, $msg)
     &$zabbixSender -z $zabbixServer -s $senderHost -k $tName -o $msg
 }
 #################################################################################################################################
-function find_expiring($certs, $interval, $trapName)
+function find_expiring($interval, $trapName)
 {
     # если $trapName пустая строка, то не посылаем в zabbix
+
+    $certs = get_certs_list
+
     $expiringsList = get_expirings_days -certs $certs
     foreach ($item in $expiringsList)
     {
@@ -261,11 +301,41 @@ function find_expiring($certs, $interval, $trapName)
     }
 }
 #################################################################################################################################
-#################################################################################################################################
-#################################################################################################################################
+function certs_sort()
+{
+    $serts = get_certs_list #
 
-# --- Получаем список сертификатов текущего пользователя
-$certs = get_current_user_certs
+    $regFilesList = Get-Childitem -Path $certsFolder #
+
+    Remove-Item $sortFolder -Recurse -Force
+    create_folder -folderName $sortFolder
+
+    foreach ($regFile in $regFilesList)
+    {
+        remove_all_certs
+        
+        import_cert_from_reg_file -regFile $regFile
+
+        $sert = get_certs_list # в списке будит только один сертификат, который импортировали выше
+
+        $subject = get_subject_params -subject $sert[0].Subject
+
+        $fio = $subject['SN'] +' '+ $subject['G']
+
+        $fioFolder = $sortFolder+'\'+$fio
+
+        create_folder -folderName $fioFolder
+
+        Copy-Item -Path $regFile.FullName -Destination $fioFolder
+    }
+
+    remove_all_certs
+
+    import_certs_from_reg_files
+}
+#################################################################################################################################
+#################################################################################################################################
+#################################################################################################################################
 
 if ($args.Count -gt 0)
 {
@@ -273,25 +343,26 @@ if ($args.Count -gt 0)
     {
         '-export'
         {
-            # --- Экспортируем все сертификаты текущего пользователя в папку
-            export_certs_from_current_user -certs $certs -certsFolder $certsFolder
+            # --- Экспортируем все сертификаты текущего пользователя в каталог
+            export_certs
         }
 
         '-import'
         {
-            # --- Импортируем все сертификаты текущему пользователю из папки
-            import_certs_to_current_user -certs $certs -certsFolder $certsFolder
+            # --- Импортируем все сертификаты текущему пользователю из каталога
+            import_certs_from_reg_files
         }
 
         '-clear'
         {
             # --- Удаляем все сертификаты у текущего пользователя
-            remove_all_certs -certs $certs
+            remove_all_certs
         }
 
         '-expiring'
         {
             # --- Выводим сертификаты у которых истекает срок действия через $interval ($args[1])
+            
             if ($args.Count -gt 1)
             {
                 $interval = $args[1]
@@ -303,29 +374,26 @@ if ($args.Count -gt 0)
                 $tName    = ''
             }
 
-            find_expiring -certs $certs -interval $interval -trapName $tName
+            find_expiring -interval $interval -trapName $tName
+        }
+
+        '-sort'
+        {
+            # --- Сортируем сертификаты по владельцу
+            
+            certs_sort
         }
     }
 }
 
-# --- Вывод параметров из Subject
-#$paramsList = get_subjects_params -certs $certs
-#foreach ($item in $paramsList)
-#{
-#    $item
-#    '--------'
-#}
+# --- 1
+#export_certs
 
-# --- Вывод количества дней действия сертификатов
-#$expiringsList = get_expiring_days -certs $certs
-#foreach ($item in $expiringsList)
-#{
-#    $item
-#    '--------'
-#}
+# --- 2
+#remove_all_certs
 
-# --- Отправка сообщения/значения трапу в zabbix 
-#send_to_zabbix -trapName $trapName -msg 'тест'
+# --- 3
+#import_certs_from_reg_files
 
-#find_expiring -certs $certs -interval $interval -trapName $trapName
-#find_expiring -certs $certs -interval 14 -trapName ''
+# --- 4
+#certs_sort
