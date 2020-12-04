98af8d9237149e5d5e27a40d0c8d33c38bf1aa41
sameza <samezarus@gmail.com>
2020-12-04 10:46:54 +1000

    Add files via upload
    Файл для бэкапов Acronis
    Есть проблема что он создаёт файлы не с именем который задаётся в задании, а с именем задания  + время и ещё что-то там.
    При копировании на НАС к примеру, это не очень удобно.
    Собственно вот скрипт
    
    # -pre  - действие перед выполнением бэкапа
    #     $backupDir - имя каталога в котором следует удалить все файлы
    #
    # -post - действие после выполнением бэкапа
    #     $backupDir         - имя каталога в котором содержится бэкап (day_2020_12_03_19_00_03_734D.TIB)
    #     $newNameBackupFile - новое имя бэкап файла
    #
    # powershell prePostAcronis.ps1 -pre c:\temp\day\
    # powershell prePostAcronis.ps1 -post c:\temp\day\ new.TIB

---
+++ prePostAcronis.ps1
@@ -0,0 +1,49 @@
+﻿# Скрипт для обработки архивов сделанных Acronis
+#
+# -pre  - действие перед выполнением бэкапа
+#     $backupDir - имя каталога в котором следует удалить все файлы
+#
+# -post - действие после выполнением бэкапа
+#     $backupDir         - имя каталога в котором содержится бэкап (day_2020_12_03_19_00_03_734D.TIB)
+#     $newNameBackupFile - новое имя бэкап файла
+#
+# powershell prePostAcronis.ps1 -pre c:\temp\day\
+# powershell prePostAcronis.ps1 -post c:\temp\day\ new.TIB 
+
+clear
+
+function pre_acronis($backupDir)
+{
+    Remove-Item $backupDir* -Recurse -Force
+}
+
+function post_acronis($backupDir, $newNameBackupFile)
+{
+    $filesList = Get-ChildItem $backupDir
+    foreach ($file in $filesList)
+    {
+        if ($file.Extension -eq ".TIB")
+        {
+            $s = $backupDir + $file.Name
+            Rename-Item -path $s -NewName $newNameBackupFile
+        }
+    }
+}
+
+#post_acronis -backupDir "C:\test\day\" -newNameBackupFile "day.TIB"
+
+if ($args.Count -gt 0)
+{
+    switch -Exact($args[0])
+    {
+        '-pre'
+        {
+            pre_acronis -backupDir $args[1]
+        }
+
+        '-post'
+        {
+            post_acronis -backupDir $args[1] -newNameBackupFile $args[2]
+        }
+    }
+}
